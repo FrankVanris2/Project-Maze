90a6fb0dda87f580a159a8356eca9e7fe9ba7782
FrankCVanris <frankcvanris@gmail.com>
2026-05-19 20:55:39 +0200

    I added the key meshes so that tomorrow I can finish up the door mechanic.

--- Items/Interactables/exit_door.gd
+++ Items/Interactables/exit_door.gd
@@ -1,5 +1,15 @@
 extends Node3D
 
+# Tracking the count
+var keys_inserted: int = 0
+
+# Variables holding the 3 key slots of the door
+@onready var slot1 = $KeySlot1
+@onready var slot2 = $KeySlot2
+@onready var slot3 = $KeySlot3
+
+# The number of slots
+@onready var door_key_slots = [slot1, slot2, slot3]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,3 +26,8 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 			get_tree().quit()
 		else:
 			print("The door is locked tight. You have ", keys_held, " out of 3 keys")
+
+
+func interact(player: CharacterBody3D) -> void:
+	print("The Door was right-clicked: ", player.name)
+	print("Current keys in door: ", keys_inserted)
